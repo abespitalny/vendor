93e65e8a7e36be071fbe8395e89b60a42493d50c
Kirk Easterson <kirk.easterson@gmail.com>
2018-12-02 11:37:48 -0500

    Just merged with master on kirk branch and added updated files. Now I'm about to merge with amster on the master branch.

--- CSE305/src/main/java/dao/BidDao.java
+++ CSE305/src/main/java/dao/BidDao.java
@@ -2,16 +2,29 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
+import org.eclipse.jdt.internal.compiler.ast.Statement;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
 
 import model.Bid;
 
 public class BidDao {
 
-	public List<Bid> getBidHistory(String auctionID) {
+	public List<Bid> getBidHistory(String auctionID1) {
+		/*
+		 * The students code to fetch data from the database 
+		 * 
+		 * Each record is required to be encapsulated as a "Bid" class object 
+		 * and added to the "bids" ArrayList auctionID, which is the Auction's 
+		 * ID, is given as method parameter 
+		 * 
+		 * Query to get the bid history of an auction, indicated by auctionID, 
+		 * must be implemented
+		 */
 
 		// Get connection
 		Connection connection = ConnectionUtils.getMyConnection();
@@ -19,28 +32,30 @@ public class BidDao {
 		// Create statement
 		Statement statement = connection.createStatement();
 
-		String sql = "SELECT \r\n" +
-				"		CustomerID, \r\n" +
-				"        AuctionID, \r\n" +
-				"        BidTime \r\n" +
-				"	FROM \r\n" +
-				"		Bid\r\n" +
-				"	WHERE \r\n" +
-				"		AuctionID = " + auctionID + "h\r\n" +
-				"	ORDER BY \r\n" +
-				"		BidTime DESC;";
+		String sql = "SELECT\r\n" +
+				"        AuctionID,\r\n" +
+				"        CustomerID,\r\n" +
+				"        BidTime,\r\n" +
+				"        BidPrice\r\n" +
+				"    FROM\r\n" +
+				"        Bid\r\n" +
+				"    WHERE\r\n" +
+				"        AuctionID = " + auctionID1 + "\r\n" +
+				"    ORDER BY\r\n" +
+				"        BidTime DESC";
 
 		ResultSet rs = statement.executeQuery(sql);
 
 		List<Bid> bids = new ArrayList<Bid>();
 
 		while (rs.next()) {
-			String customerID = Integer.toString(rs.getInt(1));
+			int auctionID2 = rs.getInt(1)
+			String customerID = rs.getString(2);
 			String bidTime = rs.getDate(3).toString();
 			float bidPrice = rs.getFloat(4);
 
 			Bid bid = new Bid();
-			bid.setAuctionID(Integer.parseInt(auctionID));
+			bid.setAuctionID(auctionID2);
 			bid.setCustomerID(customerID);
 			bid.setBidTime(bidTime);
 			bid.setBidPrice(bidPrice);
@@ -50,40 +65,82 @@ public class BidDao {
 		return bids;
 	}
 
-	///////////////////////////////////////////////////////
-	// TODO
-	public List<Bid> getAuctionHistory(String customerID) {
+	public List<Bid> getAuctionHistory(String customerID1) {
+		/*
+		 * The students code to fetch data from the database 
+		 * 
+		 * Each record is required to be encapsulated as a "Bid" class object 
+		 * and added to the "bids" ArrayList customerID, which is the Customer's 
+		 * ID, is given as method parameter 
+		 * 
+		 * Query to get the bid history of all the auctions in which a customer 
+		 * participated, indicated by customerID, must be implemented
+		 */	
+
+		// Get connection
+		Connection connection = ConnectionUtils.getMyConnection();
+
+		// Create statement
+		Statement statement = connection.createStatement();
+
+		String sql = "SELECT\r\n" +
+				"        AuctionID,\r\n" +
+				"        CustomerID,\r\n" +
+				"        BidTime,\r\n" +
+				"        BidPrice\r\n" +
+				"    FROM\r\n" +
+				"        Bid\r\n" +
+				"    WHERE\r\n" +
+				"        CustomerID = " + customerID1 + "\r\n" +
+				"    ORDER BY\r\n" +
+				"        BidTime DESC";
+
+		ResultSet rs = statement.executeQuery(sql);
 
 		List<Bid> bids = new ArrayList<Bid>();
 
-		/*
-		 * The students code to fetch data from the database Each record is
-		 * required to be encapsulated as a "Bid" class object and added to the
-		 * "bids" ArrayList customerID, which is the Customer's ID, is given as
-		 * method parameter Query to get the bid history of all the auctions in
-		 * which a customer participated, indicated by customerID, must be
-		 * implemented
-		 */
+		while (rs.next()) {
+			int auctionID = rs.getInt(1)
+			String customerID2 = rs.getString(2);
+			String bidTime = rs.getDate(3).toString();
+			float bidPrice = rs.getFloat(4);
 
-		/* Sample data begins */
-		for (int i = 0; i < 10; i++) {
 			Bid bid = new Bid();
-			bid.setAuctionID(123);
-			bid.setCustomerID("123-12-1234");
-			bid.setBidTime("2008-12-11");
-			bid.setBidPrice(100);
+			bid.setAuctionID(auctionID);
+			bid.setCustomerID(customerID2);
+			bid.setBidTime(bidTime);
+			bid.setBidPrice(bidPrice);
 			bids.add(bid);
 		}
-		/* Sample data ends */
 
 		return bids;
 	}
 
 	///////////////////////////////////////////////////////////////////////
-	// Logic isn't correct. I have to figure out how to execute SQL
-	// without requiring a resultset
+	// TODO: This might need to be rewritten
 	public Bid submitBid(String auctionID, String itemID, Float currentBid,
 			Float maxBid, String customerID) {
+		/*
+		 * The students code to insert data in the database 
+		 * 
+		 * auctionID, which is the Auction's ID for which the bid is submitted, 
+		 * is given as method parameter 
+		 * 
+		 * itemID, which is the Item's ID for which the bid is submitted, is 
+		 * given as method parameter 
+		 * 
+		 * currentBid, which is the Customer's current bid, is given as method 
+		 * parameter 
+		 * 
+		 * maxBid, which is the Customer's maximum bid for the item, is given 
+		 * as method parameter
+		 * 
+		 * customerID, which is the Customer's ID, is given as method parameter
+		 * 
+		 * Query to submit a bid by a customer, indicated by customerID, must be
+		 * implemented After inserting the bid data, return the bid details
+		 * encapsulated in "bid" object
+		 */
 
 		// Get connection
 		Connection connection = ConnectionUtils.getMyConnection();
@@ -119,20 +176,20 @@ public class BidDao {
 				+
 				"		BidIncrement = 1.00\r\n" +
 				"	WHERE \r\n" +
-				"		AuctionID = 1357;";
+				"		AuctionID = 1357";
 
 		statement.executeUpdate(sql_update); // Returns int, but not needed
 												// unless we check for errors
 
-//		String sql_result = ""; // Get result bid
-//		 I'm not 100% certain yet as to how to get the bid in a result set
-//		 UPDATE: Created Data object so we have all the data already, this
-//		 next step might not be necessary
-//
-//		ResultSet rs = statement.executeQuery(sql_result);
-//
-//		String bidTime = rs.getDate(3).toString();
-//		float bidPrice = rs.getFloat(4);
+		// String sql_result = ""; // Get result bid
+		// I'm not 100% certain yet as to how to get the bid in a result set
+		// UPDATE: Created Data object so we have all the data already, this
+		// next step might not be necessary
+		//
+		// ResultSet rs = statement.executeQuery(sql_result);
+		//
+		// String bidTime = rs.getDate(3).toString();
+		// float bidPrice = rs.getFloat(4);
 
 		Bid bid = new Bid();
 		bid.setAuctionID(Integer.parseInt(auctionID));
@@ -144,30 +201,46 @@ public class BidDao {
 	}
 
 	////////////////////////////////////////////////////////
-	// TODO: Not sure why we are returning Bid objects
+	// TODO: Not sure why we are returning a list of Bid objects. Auctions would 
+	// make more sense. I need to ask the TA.
 	public List<Bid> getSalesListing(String searchKeyword) {
+		/*
+		 * The students code to fetch data from the database 
+		 * 
+		 * Each record is required to be encapsulated as a "Bid" class object and 
+		 * added to the "bids" ArrayList searchKeyword, which is the search 
+		 * parameter, is given as method parameter 
+		 * 
+		 * Query to produce a list of sales by item name or by customer name must 
+		 * be implemented The item name or the customer name can be searched with 
+		 * the provided searchKeyword
+		 */
+		
+		// Get connection
+		Connection connection = ConnectionUtils.getMyConnection();
+
+		// Create statement
+		Statement statement = connection.createStatement();
+
+		String sql = "";
+
+		ResultSet rs = statement.executeQuery(sql);
 
 		List<Bid> bids = new ArrayList<Bid>();
 
-		/*
-		 * The students code to fetch data from the database Each record is
-		 * required to be encapsulated as a "Bid" class object and added to the
-		 * "bids" ArrayList searchKeyword, which is the search parameter, is
-		 * given as method parameter Query to produce a list of sales by item
-		 * name or by customer name must be implemented The item name or the
-		 * customer name can be searched with the provided searchKeyword
-		 */
+		while (rs.next()) {
+			int auctionID2 = rs.getInt(1)
+			String customerID = rs.getString(2);
+			String bidTime = rs.getDate(3).toString();
+			float bidPrice = rs.getFloat(4);
 
-		/* Sample data begins */
-		for (int i = 0; i < 10; i++) {
 			Bid bid = new Bid();
-			bid.setAuctionID(123);
-			bid.setCustomerID("123-12-1234");
-			bid.setBidTime("2008-12-11");
-			bid.setBidPrice(100);
+			bid.setAuctionID(auctionID2);
+			bid.setCustomerID(customerID);
+			bid.setBidTime(bidTime);
+			bid.setBidPrice(bidPrice);
 			bids.add(bid);
 		}
-		/* Sample data ends */
 
 		return bids;
 	}
