ee3a07017dd0c5f175a6e8ce0f1effdb1b61d75f
Kirk Easterson <kirk.easterson@gmail.com>
2018-12-11 07:08:44 -0500

    I forgot to add the updated Item.java file

--- CSE305/src/main/java/model/Item.java
+++ CSE305/src/main/java/model/Item.java
@@ -2,8 +2,9 @@ package model;
 
 /*
  * This class is a representation of the Item table from the schema
- */	 
+ */
 public class Item {
+
     private int itemID;
     private String name;
     private String type;
@@ -11,44 +12,70 @@ public class Item {
     private int yearManufactured;
     private int numInStock;
     private int numSold;
-    
-    public int getItemID() {
-        return itemID;
+    private int soldPrice;
+
+    public int getSoldPrice() {
+        return soldPrice;
     }
-    public void setItemID(int itemID) {
-        this.itemID = itemID;
+
+    public void setSoldPrice(int soldPrice) {
+        this.soldPrice = soldPrice;
     }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getType() {
-        return type;
-    }
-    public void setType(String type) {
-        this.type = type;
-    }
-    public int getYearManufactured() {
-        return yearManufactured;
-    }
-    public void setYearManufactured(int yearManufactured) {
-        this.yearManufactured = yearManufactured;
-    }
-    public int getNumInStock() {
-        return numInStock;
-    }
-    public void setNumInStock(int numInStock) {
-        this.numInStock = numInStock;
-    }
-    public int getNumSold() {
-        return numSold;
-    }
-    public void setNumSold(int numSold) {
-        this.numSold = numSold;
-    }
+
     public String getDescription() {
         return description;
     }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYearManufactured() {
+        return yearManufactured;
+    }
+
+    public void setYearManufactured(int yearManufactured) {
+        this.yearManufactured = yearManufactured;
+    }
+
+    public int getNumInStock() {
+        return numInStock;
+    }
+
+    public void setNumInStock(int numInStock) {
+        this.numInStock = numInStock;
+    }
+
+    public int getNumSold() {
+        return numSold;
+    }
+
+    public void setNumSold(int numSold) {
+        this.numSold = numSold;
+    }
+
 }
